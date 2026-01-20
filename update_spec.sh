191985a9c174c139ef2854c473887bf2b4aca3c3
mochizuki0323 <117917044+mochizuki0323@users.noreply.github.com>
2026-01-21 04:55:28 +0800

    Create update_spec.sh to modify mesa.spec

---
+++ update_spec.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+UPSTREAM_URL="https://raw.githubusercontent.com/vulturm/linux-graphics/master/fedora/mesa-git"
+curl -O $UPSTREAM_URL/mesa.spec
+
+SPEC_FILE="mesa.spec"
+
+
+sed -i 's/%bcond_with patented_video_codecs 0/%bcond_without patented_video_codecs/g' $SPEC_FILE
+
+sed -i 's/%define _lto_cflags %{nil}/# %define _lto_cflags %{nil}/g' $SPEC_FILE
+
+if ! grep -q "export CC=clang" $SPEC_FILE; then
+    sed -i '/^%build/a \
+export CC=clang \
+export CXX=clang++ \
+export AR=llvm-ar \
+export NM=llvm-nm \
+export RANLIB=llvm-ranlib \
+export CFLAGS="-O3 -flto=thin -pipe" \
+export CXXFLAGS="-O3 -flto=thin -pipe" \
+export LDFLAGS="-flto=thin -fuse-ld=lld"' $SPEC_FILE
+fi
+
+if ! grep -q "\--optimization=3" $SPEC_FILE; then
+    sed -i '/^%meson\([[:space:]\\]\|$\)/a \
+  --optimization=3 \\\
+  -Db_lto=true \\\
+  -Db_lto_mode=thin \\' $SPEC_FILE
+fi
+
+sed -i 's/.*-Dvideo-codecs=.*/  -Dvideo-codecs=all \\/g' $SPEC_FILE
+
+if ! grep -q "BuildRequires:  clang" $SPEC_FILE; then
+    sed -i '/BuildRequires:  meson/a BuildRequires:  clang\nBuildRequires:  lld\nBuildRequires:  llvm-devel' $SPEC_FILE
+fi
+
+sed -i 's/Release:        \(.*\)/Release:        \1.clang.lto.allcodecs/g' $SPEC_FILE
+
+echo "Success！"
