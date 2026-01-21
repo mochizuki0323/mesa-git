a878ac3d40e44d4fe93d6401b8453e7d3a89ec74
mochizuki0323 <117917044+mochizuki0323@users.noreply.github.com>
2026-01-21 09:21:26 +0800

    Clean up update_spec.sh by removing commented code
    Removed unnecessary environment variable settings and optimizations from the spec file update script.

--- update_spec.sh
+++ update_spec.sh
@@ -11,35 +11,8 @@ curl -O $UPSTREAM_URL/Mesa-MLAA-License-Clarification-Email.txt
 
 SPEC_FILE="mesa.spec"
 
-
 sed -i 's/%bcond_with patented_video_codecs 0/%bcond_without patented_video_codecs/g' $SPEC_FILE
 
-sed -i 's/%define _lto_cflags %{nil}/# %define _lto_cflags %{nil}/g' $SPEC_FILE
-
-if ! grep -q "export CC=clang" $SPEC_FILE; then
-    sed -i '/^%build/a \
-export CC=clang \
-export CXX=clang++ \
-export AR=llvm-ar \
-export NM=llvm-nm \
-export RANLIB=llvm-ranlib \
-export CFLAGS="-O3 -flto=thin -pipe" \
-export CXXFLAGS="-O3 -flto=thin -pipe" \
-export LDFLAGS="-flto=thin -fuse-ld=lld"' $SPEC_FILE
-fi
-
-if ! grep -q "\--optimization=3" $SPEC_FILE; then
-    sed -i '/^%meson\([[:space:]\\]\|$\)/a \
-  --optimization=3 \\\
-  -Db_lto=true \\\
-  -Db_lto_mode=thin \\' $SPEC_FILE
-fi
-
 sed -i 's/.*-Dvideo-codecs=.*/  -Dvideo-codecs=all \\/g' $SPEC_FILE
-
-if ! grep -q "BuildRequires:  lld" $SPEC_FILE; then
-    sed -i '/BuildRequires:  meson/a BuildRequires:  clang\nBuildRequires:  lld\nBuildRequires:  llvm-devel' $SPEC_FILE
-fi
-
 
 echo "Success！"
